1a4aa7b82e8d3a943ba7b37d033fc0c7e5b44b37
ssouzageo <ssouzageovaneo@gmail.com>
2025-01-14 17:29:48 -0300

    Botão adicionado e sua "apertar".

--- app/src/main/java/com/example/gasosacalculadora/MainActivity.kt
+++ app/src/main/java/com/example/gasosacalculadora/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.gasosacalculadora
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btniniciar = findViewById<Button>(R.id.btn_iniciar)
+
+        btniniciar.setOnClickListener {
+
+            println("Geo ação do botão ")
+        }
+
         }
     }
-}
