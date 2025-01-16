f454ac802f51decfb1d4e9ab77e054ae4ef2fca7
ssouzageo <ssouzageovaneo@gmail.com>
2025-01-15 21:16:02 -0300

    Atualização.

--- app/src/main/java/com/example/gasosacalculadora/PriceActivity.kt
+++ app/src/main/java/com/example/gasosacalculadora/PriceActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
-const val KEY_PRICE_GC = "PriceActivity.Key_GC"
+const val KEY_PREÇO_GC = "PreçoActivity.Key_GC"
 
 class PriceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,14 +17,22 @@ class PriceActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_price)
 
-        val price = intent.getFloatExtra(KEY_PRICE_GC, 0f)
+        val price = intent.getFloatExtra(KEY_PREÇO_GC, 0f)
 
-        val btnprice = findViewById<Button>(R.id.btn_price)
+        val edtPreço = findViewById<TextInputEditText>(R.id.edt_preço)
+        val btnPreço = findViewById<Button>(R.id.btn_preço)
 
-        btnprice.setOnClickListener {
+        btnPreço.setOnClickListener {
+
+            val preçoStr: String = edtPreço.text.toString()
+
+            if (preçoStr == "") {
+
+            } else {
+                val preço = edtPreço.text.toString().toFloat()
+            }
 
             println("Geo ação de botão " + price)
-
         }
 
     }
