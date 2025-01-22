cc61f0c38765da6291cbec1e360634cc5d7cee1a
ssouzageo <ssouzageovaneo@gmail.com>
2025-01-22 16:49:44 -0300

    Cabeçalho adicionado e alteração de cores.

--- app/src/main/java/com/example/gasosacalculadora/ResultActivity.kt
+++ app/src/main/java/com/example/gasosacalculadora/ResultActivity.kt
@@ -6,11 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+const val RESULT_KEY_GC = "ResultActivity.key_gc"
+
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_result)
+
+        val price = intent.getFloatExtra(KEY_PRICE_GC, 0f)
+        val consumo = intent.getFloatExtra(KEY_CONSUMO_GC, 0f)
+        val distancia = intent.getFloatExtra(KEY_DISTANCIA_GC, 0f)
+
 
 
     }
