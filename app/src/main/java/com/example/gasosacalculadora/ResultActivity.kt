c8e409f76525a0221813c4af0add0d803910352c
ssouzageo <ssouzageovaneo@gmail.com>
2025-01-24 09:16:01 -0300

    Imagem adicionada.

--- app/src/main/java/com/example/gasosacalculadora/ResultActivity.kt
+++ app/src/main/java/com/example/gasosacalculadora/ResultActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gasosacalculadora
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,7 +20,8 @@ class ResultActivity : AppCompatActivity() {
         val consumo = intent.getFloatExtra(KEY_CONSUMO_GC, 0f)
         val distancia = intent.getFloatExtra(KEY_DISTANCIA_GC, 0f)
 
-
+        val buttonResult = findViewById<Button>(R.id.btn_result)
+        val tvresult = findViewById<TextView>(R.id.tv_result)
 
     }
 }
